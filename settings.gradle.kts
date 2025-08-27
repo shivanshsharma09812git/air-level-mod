01272056653b0a16f3ec213e07446a8c4b0f17a5
shivanshsharma09812git <sharmashivansh.09812@gmail.com>
2025-08-27 11:19:29 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
+pluginManagement {
+    repositories {
+        // required so Gradle can find the fabric-loom plugin
+        maven("https://maven.fabricmc.net/")
+        // default plugin portal (keeps compatibility)
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+    }
+}
 rootProject.name = "air-cull"
