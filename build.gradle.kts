20a4c91a2f2e5cdf75ae816bbebcadf10dee0357
shivanshsharma09812git <sharmashivansh.09812@gmail.com>
2025-08-27 15:12:02 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,30 @@
 plugins {
     id("fabric-loom") version "1.7-SNAPSHOT"
     id("maven-publish")
+    java
 }
 
-val minecraftVersion = "1.21.4"
-val loaderVersion = "0.16.10"
-val fabricApiVersion = "0.111.0+1.21.4"
-
-group = "com.aircul"
+group = "com.example"
 version = "1.0.0"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
-}
-
-repositories {
-    mavenCentral()
-    maven { url = uri("https://maven.fabricmc.net/") }
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+    withSourcesJar()
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:$minecraftVersion")
-    mappings("net.fabricmc:yarn:1.21.4+build.1:v2")
-    modImplementation("net.fabricmc:fabric-loader:$loaderVersion")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:$fabricApiVersion")
-    implementation("org.spongepowered:mixin:0.8.5")
+    minecraft("com.mojang:minecraft:1.20.4")
+    mappings("net.fabricmc:yarn:1.20.4+build.3:v2")
+    modImplementation("net.fabricmc:fabric-loader:0.15.11")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.97.8+1.20.4")
 }
 
-loom {
-    runs {
-        named("client") {
-            client()
-        }
+tasks.jar {
+    archiveBaseName.set("air-cull")
+    archiveVersion.set(version.toString())
+    from("LICENSE") {
+        rename { "LICENSE_${project.name}" }
     }
-}
-
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
 }
